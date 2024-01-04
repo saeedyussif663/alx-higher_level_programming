abf80e91897085227402a9b4d1213f84a1679b88
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-04 22:54:03 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-print(len(argv))
+length = (len(argv)) - 1
+if length == 0:
+    print(f"{length} arguments.")
+elif length == 1:
+    print(f"{length} argument:")
+    print(f"{length}: {argv[length]}")
+else:
+    print(f"{length} arguments:")
+    i = 1
+    while i <= length:
+        print(f"{i}: {argv[i]}")
+        i += 1
