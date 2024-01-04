7d9ec7de0dd75506466e4747f183afb20cad2a29
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-04 23:10:28 +0000

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    length = (len(argv)) - 1
+    if length == 0:
+        print(0)
+    else:
+        total = 0
+        i = 1
+        while i <= length:
+            total += int(argv[i])
+            i += 1
+        print(total)
