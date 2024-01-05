a90a640355753e8bceb40429a438c88234149c2e
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-05 00:16:18 +0000

    fixed pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif argv[2] != "+" and argv[2] != "-" \
-and argv[2] != "*" and argv[2] != "/":
+    and argv[2] != "*" and argv[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
@@ -16,10 +16,10 @@ and argv[2] != "*" and argv[2] != "/":
         b = int(argv[3])
         operator = argv[2]
         if operator == "+":
-            print("{} {} {} = {}".format(a, operator, b, add(a,b)))
+            print("{} {} {} = {}".format(a, operator, b, add(a, b)))
         elif operator == "-":
-            print("{} {} {} = {}".format(a, operator, b, sub(a,b)))
+            print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
         elif operator == "*":
-            print("{} {} {} = {}".format(a, operator, b, mul(a,b)))
+            print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
         else:
-            print("{} {} {} = {}".format(a, operator, b, di(a,b)))
+            print("{} {} {} = {}".format(a, operator, b, div(a, b)))
