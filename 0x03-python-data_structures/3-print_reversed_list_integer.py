d397e45370cff1415b80dd1f5894b884274cf369
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-06 02:41:34 +0000

    task 0 to 4

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_list)
+
+    number_of_loop = 1
+    for element in my_list:
+        if length >= number_of_loop:
+            print("{}".format(my_list[-number_of_loop]))
+            number_of_loop += 1
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,5 @@
+# solution to the code for data structures in python.
+
+## Data Structures are:
+
+### Lists, Tuples
