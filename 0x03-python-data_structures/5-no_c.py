4302d553e64a930017eecb9941f1fe6bc392d949
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-06 06:09:02 -0800

    5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    length_of_str = len(my_string)
+    new_string = []
+    for char in range(0, length_of_str):
+        if my_string[char] != "c" and my_string[char] != "C":
+            new_string.append(my_string[char])
+        else:
+          continue
+    new_string = "".join(new_string)
+    return new_string
