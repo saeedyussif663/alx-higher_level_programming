76cabb10d3256d9d39ffa5c007598bb4526895cb
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-10 05:54:52 -0800

    1 error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    filtered_list = [i if i != search else \
-                     replace for i in my_list]
+    filtered_list = [i if i != search else replace for i in my_list]
     return(filtered_list)
