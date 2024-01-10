e6a2153fb95e56e94e2e053c63d21573cc2e0a66
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-10 13:59:46 +0000

    2 error

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from functools import reduce
 
 def uniq_add(my_list=[]):
     uniq_list = set(my_list)
-    total = reduce(lambda x, y: x + y, uniq_list)
+    total = 0
+    for i in uniq_list:
+        total += i
     return total
