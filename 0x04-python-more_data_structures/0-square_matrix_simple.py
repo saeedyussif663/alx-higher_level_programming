9f4d9b1d6b88d5563ca79cafef55fe3e790756c0
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-10 12:28:56 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    final = []
+    for item in matrix:
+        square = [i*i for i in item]
+        final.append(square)
+    return final
