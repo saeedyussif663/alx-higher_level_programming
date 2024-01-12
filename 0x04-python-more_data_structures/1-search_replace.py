69e1c4650c32e33b79d1015e155cc2bd496d91d1
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-12 02:04:39 -0800

    1 and 10 errors fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,4 +2,4 @@
 
 def search_replace(my_list, search, replace):
     filtered_list = [i if i != search else replace for i in my_list]
-    return(filtered_list)
+    return (filtered_list)
--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     highest_score = 0
     highest_score_person = ''
