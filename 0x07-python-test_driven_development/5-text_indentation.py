4160e9a73e1a0b5a616b2fae1762bf82d9c1b9d2
saeedyussif663 <saeedyussif663@gmail.com>
2024-01-31 14:26:50 +0000

    test5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,6 +12,8 @@ of these characters: ., ? and :
     Arg: text(string) The text to indent
     Return: The indented string
     """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
     global encountered_letter
     for letter in text:
         if encountered_letter:
