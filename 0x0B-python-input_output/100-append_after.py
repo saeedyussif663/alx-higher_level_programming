e9713c30b788b4bcf283cd9fc26b141d79eab9b1
saeedyussif663 <saeedyussif663@gmail.com>
2024-02-06 20:34:06 +0000

    append error

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,12 +5,13 @@ line containing a specific string
 
 
 def append_after(filename="", search_string="", new_string=""):
-    text = ""
-    with open(filename, "r") as file:
-        for line in file:
-            text += line
-            if search_string in line:
-                text += new_string
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-    with open(filename, "w") as file:
-        file.write(text)
+    for i in range(len(lines)):
+        if search_string in lines[i]:
+            lines.insert(i + 1, new_string)
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        content = "".join(lines)
+        file.write(content)
