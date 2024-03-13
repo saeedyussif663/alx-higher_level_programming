d7fe0d490c9f3605c91483c753e07991716a9164
saeedyussif663 <saeedyussif663@gmail.com>
2024-03-13 00:23:49 +0000

    mandatory done

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- Records should be ordered by score (top first)
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- ists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM second_table WHERE score >= 10
+SELECT score, name FROM second_table WHERE score >= 10
